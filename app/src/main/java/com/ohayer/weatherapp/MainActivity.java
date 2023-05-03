8dea4ae6f44012836ec8c5e3c47a7e3a2ec15ef8
ohayer <olik03.2003@wp.pl>
2023-05-03 18:19:35 +0200

    create the foundations of the project

---
+++ app/src/main/java/com/ohayer/weatherapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ohayer.weatherapp;
+
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.helloWorld);
+        textView.setText("Hello");
+    }
+}
